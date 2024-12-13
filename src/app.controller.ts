fe54dffc3102e27cdf84d50b5c34cfcf8b967a2b
Khyati Soni <khyatis2511@gmail.com>
2024-12-13 17:12:04 +0530

    Put Request Added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Put(':id')
+  eventVendorAction(@Body() updateDto: any, @Param('id') id: string) {
+    return this.appService.updateAction({ updateDto, id });
   }
 }
